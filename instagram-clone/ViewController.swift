e6a1f3179268ff554473a71c43c3e055d6f786e0
Johnny Chau <jchau87@gmail.com>
2015-10-28 21:15:49 -0700

    implemented following

--- instagram-clone/ViewController.swift
+++ instagram-clone/ViewController.swift
@@ -23,7 +23,7 @@ class ViewController: UIViewController {
     
     let alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
     
-    alert.addAction(UIAlertAction(title: "ok", style: .Default, handler: { (action) -> Void in
+    alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: { (action) -> Void in
       self.dismissViewControllerAnimated(true, completion: nil)
     }))
     
@@ -31,21 +31,34 @@ class ViewController: UIViewController {
     
   }
   
+  func showLoadingSpinner() {
+    activityIndicator = UIActivityIndicatorView(frame: CGRectMake(0, 0, 50, 50))
+    activityIndicator.center = self.view.center
+    activityIndicator.hidesWhenStopped = true
+    activityIndicator.activityIndicatorViewStyle = .Gray
+    view.addSubview(activityIndicator)
+    activityIndicator.startAnimating()
+    UIApplication.sharedApplication().beginIgnoringInteractionEvents()
+  }
+  
+  func hideLoadingSpinner() {
+    self.activityIndicator.stopAnimating()
+    UIApplication.sharedApplication().endIgnoringInteractionEvents()
+  }
+
+  func inputsAreValid() -> Bool {
+    return !(username.text == "" || password.text == "")
+  }
+  
   @IBAction func signUp(sender: AnyObject) {
     
-    if username.text == "" || password.text == "" {
+    if !inputsAreValid() {
       
       displayAlert("Error in form", message: "Please enter a username and password")
       
     } else {
       
-      activityIndicator = UIActivityIndicatorView(frame: CGRectMake(0, 0, 50, 50))
-      activityIndicator.center = self.view.center
-      activityIndicator.hidesWhenStopped = true
-      activityIndicator.activityIndicatorViewStyle = .Gray
-      view.addSubview(activityIndicator)
-      activityIndicator.startAnimating()
-      UIApplication.sharedApplication().beginIgnoringInteractionEvents()
+      showLoadingSpinner()
       
       let user = PFUser()
       user.username = username.text
@@ -54,17 +67,15 @@ class ViewController: UIViewController {
       var errorMessage = "Please try again later"
       user.signUpInBackgroundWithBlock({ (success, error) -> Void in
         
-        self.activityIndicator.stopAnimating()
-        UIApplication.sharedApplication().endIgnoringInteractionEvents()
+        self.hideLoadingSpinner()
         
         if error == nil {
           // signup successful
+          self.performSegueWithIdentifier("login", sender: self)
         } else {
           
           if let errorString = error!.userInfo["error"] as? String {
-            
             errorMessage = errorString
-            
           }
           
           self.displayAlert("Failed Signup", message: errorMessage)
@@ -78,12 +89,44 @@ class ViewController: UIViewController {
   }
   
   @IBAction func login(sender: AnyObject) {
-    
+    if !inputsAreValid() {
+      
+      displayAlert("Error in form", message: "Please enter a username and password")
+      
+    } else {
+      
+      showLoadingSpinner()
+      
+      PFUser.logInWithUsernameInBackground(username.text!, password: password.text!, block:
+        { (user, error) -> Void in
+          self.hideLoadingSpinner()
+          
+          if user != nil {
+            // Logged in
+            self.performSegueWithIdentifier("login", sender: self)
+          } else {
+            var errorMessage = "Please try again later"
+            if let errorString = error!.userInfo["error"] as? String {
+              errorMessage = errorString
+            }
+            
+            self.displayAlert("Failed Signup", message: errorMessage)
+            
+          }
+          
+        })
+    }
   }
   
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+  }
+  
+  override func viewDidAppear(animated: Bool) {
+    if PFUser.currentUser() != nil {
+      self.performSegueWithIdentifier("login", sender: self)
+    }
   }
 
   override func didReceiveMemoryWarning() {
