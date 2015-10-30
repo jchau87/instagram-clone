d51915a3aa773df2b587ff2044da8dd65db4a4ef
Johnny Chau <jchau87@gmail.com>
2015-10-30 00:40:03 -0700

    WIP Feed view

---
+++ instagram-clone/TableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  TableViewCell.swift
+//  instagram-clone
+//
+//  Created by Johnny Chau on 10/29/15.
+//  Copyright © 2015 Chowfun. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+  @IBOutlet weak var postedImage: UIImageView!
+  @IBOutlet weak var message: UILabel!
+  
+  @IBOutlet weak var username: UILabel!
+}
